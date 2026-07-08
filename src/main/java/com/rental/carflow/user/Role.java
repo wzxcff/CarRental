3c01a4ba4f73213ebeaff300fc97ff2b9b5ab87c
wzxcff <Shevasfloyd@gmail.com>
2026-07-08 13:46:46 +0200

    feat: add user entity and role enum

---
+++ src/main/java/com/rental/carflow/user/Role.java
@@ -0,0 +1,7 @@
+package com.rental.carflow.user;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_MECHANIC,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/rental/carflow/user/User.java
@@ -0,0 +1,37 @@
+package com.rental.carflow.user;
+
+import jakarta.persistence.*;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = OffsetDateTime.now();
+    }
+
+
+
+}
