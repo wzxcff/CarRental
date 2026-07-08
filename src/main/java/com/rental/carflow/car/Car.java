4555cf52bbbd2ef0a69bb7714bfb00e3d0ccd63e
wzxcff <Shevasfloyd@gmail.com>
2026-07-08 14:03:49 +0200

    feat: add car entity

---
+++ src/main/java/com/rental/carflow/car/Car.java
@@ -0,0 +1,38 @@
+package com.rental.carflow.car;
+
+import com.rental.carflow.location.Location;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id", nullable = false)
+    private Location location;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(name = "license_plate", nullable = false, unique = true)
+    private String licensePlate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CarType type;
+
+    @Column(nullable = false)
+    private BigDecimal pricePerDay;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
